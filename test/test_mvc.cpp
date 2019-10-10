599a951245a9b38a631a09f1d5302025024d2d62
Andreas Pfeil <andreas_pfeil@yahoo.de>
2019-10-10 11:15:43 +0200

    implementing minimal parts of Model_Node to instantiate

--- test/test_mvc.cpp
+++ test/test_mvc.cpp
@@ -10,16 +10,39 @@ public:
     ~test_Value_Node(){}
 
 private slots:
+    // Test Model_Node callback mechanism
     void test_callback_object();
     void test_callback_lambda();
     void test_multiple_callbacks();
 
+/*
+    // Test Tree_Model_Node tree setup
+    void test_no_parent();
+    void test_parent_build_path();
+*/
+
+    // Test Value_Node value storage
     void test_set_get_value();
     void test_value_set_notifies();
 
-private:
-
+/*
+    // Test Model_Node notification?? -> best on Value_Node
+    void test_no_parent_value_int();
+    void test_no_parent_value_bool();
+    void test_no_parent_value_float();
+    void test_no_parent_value_string();
+*/
 };
+
+class testable_Model_Node : public Model_Node
+{
+    virtual void build_topic(ostream& s) const override;
+};
+
+void testable_Model_Node::build_topic(Model_Node::ostream&) const
+{
+}
+
 
 struct Counting_Callback
 {
@@ -36,9 +59,10 @@ struct Counting_Callback
     int call_counter = 0;
 };
 
+
 void test_Value_Node::test_callback_object()
 {
-    Model_Node n;
+    testable_Model_Node n;
     Counting_Callback callback_by_reference, callback_by_value;
 
     // This demonstrates how to pass a function object as callback and let the members stay accessible
@@ -53,7 +77,7 @@ void test_Value_Node::test_callback_object()
 
 void test_Value_Node::test_callback_lambda()
 {
-    Model_Node n;
+    testable_Model_Node n;
     int call_counter = 0;
 
     // This shows how to register a lambda as callback
@@ -66,7 +90,7 @@ void test_Value_Node::test_callback_lambda()
 
 void test_Value_Node::test_multiple_callbacks()
 {
-    Model_Node n;
+    testable_Model_Node n;
     int callback_signal = 0;
 
     n.register_change_callback([&callback_signal](const Model_Node&){callback_signal = 5;});
