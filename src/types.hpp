446bc4b770f0bb82f9e5bf17ea0b52f1ef6a3ebe
Andreas Pfeil <andreas_pfeil@yahoo.de>
2019-10-20 23:01:35 +0200

    redo Arduino compatilibilty changes

--- src/types.hpp
+++ src/types.hpp
@@ -1,7 +1,7 @@
 #ifndef TYPES_HPP_INCLUDED
 #define TYPES_HPP_INCLUDED
 
-#include <string>
-typedef std::string string;
+#include <Arduino.h>
+typedef String string;
 
 #endif // TYPES_HPP_INCLUDED
