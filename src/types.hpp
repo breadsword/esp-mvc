a6ecffb56b6697d2428accdf16d4caa9613a39a4
Andreas Pfeil <andreas_pfeil@yahoo.de>
2019-10-25 09:23:48 +0200

    use std::string instead of String

--- src/types.hpp
+++ src/types.hpp
@@ -1,7 +1,8 @@
 #ifndef TYPES_HPP_INCLUDED
 #define TYPES_HPP_INCLUDED
 
+#include <string>
 #include <Arduino.h>
-typedef String string;
+typedef std::string string;
 
 #endif // TYPES_HPP_INCLUDED
