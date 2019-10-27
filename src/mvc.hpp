a4c73847e608c8823e863209545122a80f4964f6
Andreas Pfeil <andreas_pfeil@yahoo.de>
2019-10-27 21:03:40 +0100

    reformat source

--- src/mvc.hpp
+++ src/mvc.hpp
@@ -10,15 +10,15 @@
 // std::cout << m.notification()
 // and perform the output using expression templates
 
-
 class Model_Node
 {
 public:
     Model_Node()
-    {}
-    virtual ~Model_Node(){}
+    {
+    }
+    virtual ~Model_Node() {}
 
-    typedef std::function<void(const Model_Node&)> callback_t;
+    typedef std::function<void(const Model_Node &)> callback_t;
     void register_change_callback(callback_t);
     void notify_subscribers() const;
 
@@ -29,45 +29,47 @@ public:
 protected:
     typedef fixedbufstream<64> ostream;
     // make these function protected to allow changing the std::ostream interface
-    virtual void build_topic(ostream&) const = 0;
-    virtual void add_value_string(ostream&) const;
+    virtual void build_topic(ostream &) const = 0;
+    virtual void add_value_string(ostream &) const;
 };
 
 class Tree_Model_Node : public Model_Node
 {
 public:
-    Tree_Model_Node(string _topic = "", Tree_Model_Node *_parent=nullptr);
+    Tree_Model_Node(string _topic = "", Tree_Model_Node *_parent = nullptr);
 
     constexpr static auto separator = "/";
     const string topic;
-    Tree_Model_Node* parent;
+    Tree_Model_Node *parent;
 
-    std::vector<std::reference_wrapper<Tree_Model_Node> > children;
+    std::vector<std::reference_wrapper<Tree_Model_Node>> children;
 
-    bool operator==(const Tree_Model_Node& rhs) const;
+    bool operator==(const Tree_Model_Node &rhs) const;
 
-    void on_tree_execute(std::function<void(const Tree_Model_Node&)> f) const;
+    void on_tree_execute(std::function<void(const Tree_Model_Node &)> f) const;
 
 protected:
-    virtual void build_topic(ostream&) const override;
+    virtual void build_topic(ostream &) const override;
 };
 
-
 template <typename T>
-class Value_Model : public Tree_Model_Node{
+class Value_Model : public Tree_Model_Node
+{
 public:
     typedef T value_t;
     Value_Model(T _value, string _topic, Tree_Model_Node *_parent = nullptr) : Tree_Model_Node{_topic, _parent}, value{_value}
-    {}
+    {
+    }
 
     Value_Model() : Tree_Model_Node{}, value{}
-    {}
+    {
+    }
 
     void set(T);
     T get() const;
 
 protected:
-    virtual void add_value_string(ostream&) const override;
+    virtual void add_value_string(ostream &) const override;
 
 private:
     T value;
@@ -76,8 +78,11 @@ private:
 template <typename T>
 void Value_Model<T>::set(T _value)
 {
-    value = _value;
-    notify_subscribers();
+    if (_value != value)
+    {
+        value = _value;
+        notify_subscribers();
+    }
 }
 
 template <typename T>
@@ -86,8 +91,8 @@ T Value_Model<T>::get() const
     return value;
 }
 
-template<typename T>
-void Value_Model<T>::add_value_string(ostream& s) const
+template <typename T>
+void Value_Model<T>::add_value_string(ostream &s) const
 {
     s << value;
 }
